c30e05ce82720836ed716ef76cca2100cc123515
Rio Kwok <riotkkwok@gmail.com>
2016-04-20 20:24:38 +0800

    under development

--- src/bulletText.js
+++ src/bulletText.js
@@ -11,18 +11,60 @@
         elm: function(selector){
             return document.querySelectorAll(selector);
         },
-        ,
+        extendObj: function(sObj, tObj){
+            for(var key in tObj){
+                if(tObj.hasOwnProperty(key)){
+                    sObj[key] = tObj[key];
+                }
+            }
+        },
+        countBytes: function(str) {
+            var totalLength = 0;
+            var charCode;
+            for (var i = 0; i < str.length; i++) {
+                charCode = str.charCodeAt(i);
+                if (charCode <= 0x007f) {
+                    totalLength += 1;
+                } else if (charCode <= 0x07ff) {
+                    totalLength += 2;
+                } else if (charCode <= 0xffff) {
+                    totalLength += 3;
+                } else {
+                    totalLength += 4;
+                }
+            }
+            return totalLength;
+        }
+    },
+    w = {
+        width: window.innerWidth,
     },
     tmpl = {
         track: '<div class="bulletT-track"></div>',
-        text: '<span class="bulletT-text" style="margin-left:{{left}};">{{text}}</span>',
+        // text: '<span class="bulletT-text" style="margin-left:{{left}};">{{text}}</span>',
+        text: '<span class="bulletT-text" style="transition:{{trst}};">{{text}}</span>',
+    },
+    options = {
+        container: '#bulletArea',
+        lines: 1,
+        discard: true, // discard the bullet cannot be handle at the same time
+        discardRule: 1, // take effect only when discard is true; 1 - random, 2 - first come first serve, 3 - last come first serve
+        charLimit: 50,
+        speed: 5000, // ms
     };
-    options = {};
 
-    var config = {};
+    function renderLines(num){
+        var str = '';
+        for(var i=0; i<num; i++){
+            str += track;
+        }
+        u.elm(container).innerHTML = str;
+    }
+
     var bulletText = {
         init: function(opt){
-            ;
+            extendObj(options, opt);
+            w.width = u.elm(options.container).innerWidth;
         },
         fire: function(bullets){},
     };
